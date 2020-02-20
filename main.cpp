ab8925eff07d8450dc41754c80c07481dc74772e
Geese Spadassin <geesespadassin@localhost.localdomain>
2020-02-20 20:39:38 +0100

    kdo

--- main.cpp
+++ main.cpp
@@ -3,12 +3,47 @@
 #include <unordered_map>
 #include <string>
 #include <algorithm>
-#include <utils>
 #include <iostream>
 #include <fstream>
 #include <set>
 #include <unordered_set>
 
-int main() {
+using namespace std;
 
+void parser();
+
+int main() {
+	int nb_books;
+	int nb_libraries;
+
+	cin >> nb_books;
+	cin >> nb_libraries;
+	cin >> Days;
+
+	for (auto i = 0; i < nb_books; ++i) {
+		int bscore;
+		cin >> bscore;
+		Book book(i, bscore, nullptr);
+		books.push_back(book);
+	}
+	for (auto i = 0; i < nb_libraries; ++i) {
+		int lcap;
+		int lsignup;
+		int lship;
+		cin >> lcap;
+		cin >> lsignup;
+		cin >> lship;
+		vector<Book*> booksptrvec;
+		for (auto j = 0; j < lcap; ++j) {
+			int id;
+			cin >> id;
+			Book* booksptr = &books[i];
+			booksptrvec.push_back(booksptr);
+		}
+		Library library(i, lcap, lsignup, lship, booksptrvec);
+		libraries.push_back(library);
+		for (auto j = 0; j < lcap; ++j) {
+			library.books[i] = &library;
+		}
+	}
 }
